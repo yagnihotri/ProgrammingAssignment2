45b13c49b5eedb79817873462bdd3b669a0a47f7
yagnihotri <yagnihotri@gmail.com>
2017-02-24 23:52:56 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+# MakeCacheMatrix creates a special object which is actually a list. 
+# This special object saves environment(parent) where "inverse" object is saved 
+# When cacheSolve is called it checks for whether the "inverse" object already exist
+# in parent ( globalenviron). If the "inverse" object exist in global envrironment, it is returned otherwise
+# inverse is calculated and "set" in the global environment to be retreived later as needed.
 
-## Write a short comment describing this function
+
+## Create a special object that is list of functions to get data, set inverse and get inverse functions
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  get <- function() x
+  setinv <- function(inver) i <<- inver
+  getinv <- function() i
+  list( get = get,
+       setinv = setinv,
+       getinv = getinv)
 
+  
 }
 
 
-## Write a short comment describing this function
+## function to check if inverse matrix already exists in cache, otherwise calculate and return it. 
+## Argument to cacheSolve is list from makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
         ## Return a matrix that is the inverse of 'x'
 }
